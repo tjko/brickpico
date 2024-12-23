dcdb635cb868aa06c130044f2d51da78b7277cfc
Timo Kokkonen <tjko@iki.fi>
2024-12-22 20:36:38 -0800

    Update pico_telnetd and ss_oled-lib libraries.

--- src/display_oled.c
+++ src/display_oled.c
@@ -94,7 +94,7 @@ void oled_display_init()
 	do {
 		sleep_ms(50);
 		res = oledInit(&oled, dtype, -1, flip, invert, I2C_HW,
-			SDA_PIN, SCL_PIN, -1, 1000000L);
+			SDA_PIN, SCL_PIN, -1, 1000000L, false);
 	} while (res == OLED_NOT_FOUND && retries++ < 10);
 
 	if (res == OLED_NOT_FOUND) {
